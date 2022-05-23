36b91d14d251358cc77b5beed4a5ef792510cb71
Bontecoin <99731565+bontecoin@users.noreply.github.com>
2022-05-23 09:04:14 +0800

    v1.0.1

--- masternodeinstall.sh
+++ masternodeinstall.sh
@@ -3,17 +3,13 @@
 PORT=3094
 RPCPORT=3095
 CONF_DIR=~/.bontecoin
+COINZIP='https://github.com/bontecoin/BONTE/releases/download/v1.0.1/bonte-linux1.0.1.zip'
 
 cd ~
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
-if [[ $(lsb_release -d) = *18.04* ]]; then
-  COINZIP='https://github.com/bontecoin/BONTE/releases/download/v1.0/bonte_ubuntu18.zip'
-fi
-if [[ $(lsb_release -d) = *20.04* ]]; then
-  COINZIP='https://github.com/bontecoin/BONTE/releases/download/v1.0/bonte_ubuntu20.zip'
-fi
+
 if [[ $EUID -ne 0 ]]; then
    echo -e "${RED}$0 must be run as root.${NC}"
    exit 1
@@ -51,31 +47,17 @@ DOSETUP="y"
 
 if [ $DOSETUP = "y" ]  
 then
-  sudo apt-get update && sudo apt-get upgrade -y
-  sudo apt-get install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils python3 libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-test-dev libboost-thread-dev libboost-all-dev libboost-program-options-dev libminiupnpc-dev libzmq3-dev libprotobuf-dev protobuf-compiler unzip software-properties-common cmake zip screen htop curl -y
-  sudo apt-get install software-properties-common -y && sudo add-apt-repository ppa:pivx/pivx -y && sudo apt-get update && sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
-
-  cd /var
-  sudo touch swap.img
-  sudo chmod 600 swap.img
-  sudo dd if=/dev/zero of=/var/swap.img bs=1024k count=2000
-  sudo mkswap /var/swap.img
-  sudo swapon /var/swap.img
-  sudo free
-  sudo echo "/var/swap.img none swap sw 0 0" >> /etc/fstab
-  cd
-  
   cd /usr/local/bin/
   wget $COINZIP
   unzip *.zip
   chmod +x bontecoin*
-  rm bontecoin-qt bontecoin-tx *.zip
+  rm bontecoin-qt bontecoin-tx bonte-linux1.0.1.zip
   
   mkdir -p $CONF_DIR
   cd $CONF_DIR
-  wget http://bontecoin.com/snapshot.zip
-  unzip snapshot.zip
-  rm snapshot.zip
+  wget http://cdn.delion.xyz/bonte.zip
+  unzip bonte.zip
+  rm bonte.zip
 
 fi
 
@@ -96,8 +78,6 @@ fi
   echo "daemon=1" >> bontecoin.conf_TEMP
   echo "maxconnections=250" >> bontecoin.conf_TEMP
   echo "masternode=1" >> bontecoin.conf_TEMP
-  echo "dbcache=100" >> bontecoin.conf_TEMP
-  echo "maxorphantx=5" >> bontecoin.conf_TEMP
   echo "" >> bontecoin.conf_TEMP
   echo "port=$PORT" >> bontecoin.conf_TEMP
   echo "externalip=$IP:$PORT" >> bontecoin.conf_TEMP
